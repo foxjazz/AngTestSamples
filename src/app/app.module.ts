224841395b3ababc2b178fd40afb3b0151ec86ed
foxjazz <fox21@foxjazz.net>
2020-04-15 19:47:54 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputStuffComponent } from './input-stuff/input-stuff.component';
+import { ControlStuffComponent } from './control-stuff/control-stuff.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputStuffComponent,
+    ControlStuffComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
